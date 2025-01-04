67959988a37994a7327b45bcf80908c7bd02ed9c
howesrichard <82419531+howesrichard@users.noreply.github.com>
2025-01-04 00:42:08 +0000

    Add interpolation to zero_curve

--- yield_curve/zero_curve.py
+++ yield_curve/zero_curve.py
@@ -4,25 +4,65 @@
 # - add_zero_rate(maturity, zero_rate): add a zero rate to the curve
 # - get_zero_rate(maturity): get the zero rate for a given maturity
 
+import numpy as np
+import math
+
 class ZeroCurve:
     def __init__(self):
         self.maturities = []
         self.zero_rates = []
+        self.AtMats = []
         self.discount_factors = []
     
     def add_zero_rate(self, maturity, zero_rate):
         self.maturities.append(maturity)
         self.zero_rates.append(zero_rate)
-        self.discount_factors.append(1/(1+zero_rate)**maturity)
+        self.AtMats.append(math.exp(zero_rate*maturity))
+        self.discount_factors.append(1/self.AtMats[-1])
     
-    def get_zero_rate(self, maturity):
+    def get_AtMat(self, maturity):
         if maturity in self.maturities:
-            return self.zero_rates[self.maturities.index(maturity)]
+            return self.AtMats[self.maturities.index(maturity)]
         else:
-            return None
+            return exp_interp(self.maturities, self.AtMats, maturity)
 
     def get_discount_factor(self, maturity):
         if maturity in self.maturities:
             return self.discount_factors[self.maturities.index(maturity)]
         else:
-            return None
+            return exp_interp(self.maturities, self.discount_factors, maturity)
+
+    def get_zero_rate(self, maturity):
+        if maturity in self.maturities:
+            return self.zero_rates[self.maturities.index(maturity)]
+        else:
+            return math.log(self.get_AtMat(maturity))/maturity
+
+        
+def exp_interp(xs, ys, x):
+    """
+    Interpolates a single point for a given value of x using continuously compounded rates.
+
+    Parameters:
+    xs (list or np.array): Vector of x values.
+    ys (list or np.array): Vector of y values.
+    x (float): The x value to interpolate.
+
+    Returns:
+    float: Interpolated y value.
+    """
+    xs = np.array(xs)
+    ys = np.array(ys)
+    
+    # Find the interval [x0, x1] where x0 <= x <= x1
+    idx = np.searchsorted(xs, x) - 1
+    x0, x1 = xs[idx], xs[idx + 1]
+    y0, y1 = ys[idx], ys[idx + 1]
+    
+    # Calculate the continuously compounded rate
+    rate = (np.log(y1) - np.log(y0)) / (x1 - x0)
+    
+    # Interpolate the y value for the given x
+    y = y0 * np.exp(rate * (x - x0))
+    
+    return y
